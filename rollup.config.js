42c0e4e817f5fb1e87a9397ca7c7bcd5e2dd07e3
Pierre Bertet <hello@pierre.world>
2020-01-21 15:30:55 +0000

    Build fixes (#11)
    * Mark the package as side-effect free
    
    * Compress build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import babel from 'rollup-plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import json from '@rollup/plugin-json'
 import builtins from 'builtin-modules'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/index.js',
@@ -23,5 +24,6 @@ export default {
     }),
     commonjs(),
     json(),
+    terser(),
   ],
 }
