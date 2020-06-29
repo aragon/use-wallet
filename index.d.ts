34b4c5006c52cf1bea209ba34043a2ad2c3370b6
James Lefrère <james@colony.io>
2020-06-29 10:11:15 +0200

    Remove semis

--- index.d.ts
+++ index.d.ts
@@ -2,67 +2,67 @@ declare module 'use-wallet' {
   import { ReactNode } from 'react';
 
   type Connectors = Partial<{
-    authereum: {};
-    fortmatic: { apiKey: string };
-    frame: {};
-    injected: {};
-    portis: { dAppId: string };
-    squarelink: { clientId: string; options: object };
+    authereum: {}
+    fortmatic: { apiKey: string }
+    frame: {}
+    injected: {}
+    portis: { dAppId: string }
+    squarelink: { clientId: string; options: object }
     torus: {
       chainId?: number;
       initOptions: object;
       constructorOptions: object;
-    };
-    walletconnect: { rpcUrl: string };
-    walletlink: { url: string; appName: string; appLogoUrl: string };
-  }>;
+    }
+    walletconnect: { rpcUrl: string }
+    walletlink: { url: string; appName: string; appLogoUrl: string }
+  }>
 
   export interface Wallet<T> {
-    account: string | null;
-    activate(connectorId: keyof Connectors): Promise<void>;
-    activated: keyof Connectors;
-    activating: boolean;
-    balance: string;
-    connected: boolean;
-    connectors: Connectors;
-    deactivate(): void;
-    chainId: number | null;
-    ethereum: T;
-    getBlockNumber(): number;
-    isContract: boolean;
-    networkName: string;
+    account: string | null
+    activate(connectorId: keyof Connectors): Promise<void>
+    activated: keyof Connectors
+    activating: boolean
+    balance: string
+    connected: boolean
+    connectors: Connectors
+    deactivate(): void
+    chainId: number | null
+    ethereum: T
+    getBlockNumber(): number
+    isContract: boolean
+    networkName: string
   }
 
   interface UseWalletProviderProps {
-    chainId: number;
-    children: ReactNode;
-    connectors?: Connectors;
-    pollBalanceInterval?: number;
-    pollBlockNumberInterval?: number;
+    chainId: number
+    children: ReactNode
+    connectors?: Connectors
+    pollBalanceInterval?: number
+    pollBlockNumberInterval?: number
   }
 
   interface UseWalletProps {
-    pollBalanceInterval?: number;
-    pollBlockNumberInterval?: number;
+    pollBalanceInterval?: number
+    pollBlockNumberInterval?: number
   }
 
   export class UnsupportedChainError extends Error {
-    name: 'UnsupportedChainError';
+    name: 'UnsupportedChainError'
   }
 
   export class UnsupportedConnectorError extends Error {
-    name: 'UnsupportedConnectorError';
+    name: 'UnsupportedConnectorError'
   }
 
   export class RejectedActivationError extends Error {
-    name: 'RejectedActivationError';
+    name: 'RejectedActivationError'
   }
 
   export class ConnectorConfigError extends Error {
-    name: 'ConnectorConfigError';
+    name: 'ConnectorConfigError'
   }
 
-  export function useWallet<T>(props?: UseWalletProps): Wallet<T>;
+  export function useWallet<T>(props?: UseWalletProps): Wallet<T>
 
-  export function UseWalletProvider(props: UseWalletProviderProps);
+  export function UseWalletProvider(props: UseWalletProviderProps)
 }
