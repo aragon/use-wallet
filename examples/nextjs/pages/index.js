2071e110d6ab74ff6f635eb3b6ddf9df128fb592
James Lefrère <james@colony.io>
2020-06-29 13:39:49 +0200

    Add Aragon RPC url to connectors

--- examples/nextjs/pages/index.js
+++ examples/nextjs/pages/index.js
@@ -105,8 +105,8 @@ function HomePage() {
       connectors={{
         fortmatic: { apiKey: '' },
         portis: { dAppId: '' },
-        walletconnect: { rpcUrl: '' },
-        walletlink: { url: '' },
+        walletconnect: { rpcUrl: 'wss://mainnet.eth.aragon.network/ws' },
+        walletlink: { url: 'wss://mainnet.eth.aragon.network/ws' },
       }}
     >
       <App />
--- examples/simple-connect/index.js
+++ examples/simple-connect/index.js
@@ -109,8 +109,8 @@ ReactDOM.render(
     connectors={{
       fortmatic: { apiKey: '' },
       portis: { dAppId: '' },
-      walletconnect: { rpcUrl: '' },
-      walletlink: { url: '' },
+      walletconnect: { rpcUrl: 'wss://mainnet.eth.aragon.network/ws' },
+      walletlink: { url: 'wss://mainnet.eth.aragon.network/ws' },
     }}
   >
     <App />
