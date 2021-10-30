4421503c572cd4ba2cde5cd8dd646406ceada312
sepehr <sepehr2mail@gmail.com>
2021-10-31 00:54:08 +0330

    change each with forEach

--- src/connectors/ConnectorWalletConnect.ts
+++ src/connectors/ConnectorWalletConnect.ts
@@ -20,7 +20,7 @@ export default async function init(): Promise<Connector> {
           'The WalletConnect connector requires rpcUrl to be set.'
         )
       }
-      Object.values(rpc).each((url: string) => {
+      Object.values(rpc).forEach((url: string) => {
         if (!/^https?:\/\//.test(url)) {
           throw new ConnectorConfigError(
             'The WalletConnect connector requires rpcUrl to be an HTTP URL.'
