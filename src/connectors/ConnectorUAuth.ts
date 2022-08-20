97f8451f803e333caa3acfb3b397de23cfd4373b
Dogu Taskiran <hasandogu@gmail.com>
2022-08-19 21:01:26 -0700

    Add UAuth connector

---
+++ src/connectors/ConnectorUAuth.ts
@@ -0,0 +1,63 @@
+import { AbstractConnector } from '@web3-react/abstract-connector'
+import { Connector } from '../types'
+import { ConnectorConfigError } from '../errors'
+
+export default async function init(): Promise<Connector> {
+  const { UAuthConnector } = await import('@uauth/web3-react')
+  return {
+    web3ReactConnector({
+      clientID,
+      redirectUri,
+      postLogoutRedirectUri,
+      scope,
+      injectedConnector,
+      walletconnectConnector,
+      shouldLoginWithRedirect,
+    }: {
+      clientID: string
+      redirectUri: string
+      postLogoutRedirectUri: string
+      scope: string
+      injectedConnector: AbstractConnector
+      walletconnectConnector: AbstractConnector
+      shouldLoginWithRedirect?: boolean
+    }) {
+      if (!clientID) {
+        throw new ConnectorConfigError(
+          'The UAuth connector requires clientID to be set.'
+        )
+      }
+
+      if (shouldLoginWithRedirect) {
+        if (!redirectUri) {
+          throw new ConnectorConfigError(
+            'The UAuth connector configuration requires redirectUri to be set.'
+          )
+        }
+
+        if (!postLogoutRedirectUri) {
+          throw new ConnectorConfigError(
+            'The UAuth connector configuration requires postLogoutRedirectUri to be set.'
+          )
+        }
+      }
+
+      return new UAuthConnector({
+        clientID,
+
+        redirectUri,
+        postLogoutRedirectUri,
+
+        scope,
+
+        // Injected and walletconnect connectors are required.
+        connectors: {
+          injected: injectedConnector,
+          walletconnect: walletconnectConnector,
+        },
+
+        shouldLoginWithRedirect,
+      })
+    },
+  }
+}
