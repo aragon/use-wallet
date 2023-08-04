ef1e4377110d8f70bd360d6a5b5f73a15a14c893
0xGabi <gabrielpk.18@gmail.com>
2023-08-03 22:59:01 -0300

    feat: add optimism chain

--- src/chains.ts
+++ src/chains.ts
@@ -108,6 +108,18 @@ const CHAIN_INFORMATION = new Map<number, ChainInformation | ChainType>([
     },
   ],
   [
+    10,
+    {
+      id: 10,
+      nativeCurrency: ETH,
+      type: 'optimism',
+      fullName: 'OP Mainnet',
+      shortName: 'Optimism',
+      explorerUrl: `https://optimistic.etherscan.io/`,
+      testnet: false,
+    },
+  ],
+  [
     11155111,
     {
       id: 11155111,
